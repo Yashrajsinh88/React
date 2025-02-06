11b9ca28f06dc40524c58b2eeb0a2056d5d6b936
Yashrajsinh <yashrajsinhdodiya888@gmail.com>
2025-02-06 12:11:41 +0530

    react

---
+++ Project/integrate template/integrate template/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import Section from './components/Section'
+import Footer from './components/Footer'
+
+function App() {
+ 
+  return (
+    <>
+      <Header/>
+      <Section/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
